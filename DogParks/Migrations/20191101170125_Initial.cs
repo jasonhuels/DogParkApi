d66bcb87ae37103e17ecb6faffbefbf08ce17a3a
Jason Huels <jasonhuels@yahoo.com>
2019-11-01 10:03:35 -0700

    add seed data and first migration

---
+++ DogParks/Migrations/20191101170125_Initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BuildAnApi.Solution.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DogParks",
+                columns: table => new
+                {
+                    DogParkId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Area = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Hours = table.Column<string>(nullable: true),
+                    PhotoPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DogParks", x => x.DogParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "DogParks",
+                columns: new[] { "DogParkId", "Address", "Area", "Hours", "Name", "PhotoPath" },
+                values: new object[,]
+                {
+                    { 1, "N Fremont Street and Interstate Avenue", "North", "5AM-12AM", "Overlook Park", "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=250" },
+                    { 2, "NW 19th Avenue and Glisan Street", "Northwest", "5AM-12AM", "Couch Park", "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=76" },
+                    { 3, "SE Cesar E Chavez Blvd and Stark Street", "Southeast", "5AM-10:30PM", "Laurelhurst Park", "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=115" },
+                    { 4, "SW 27th Avenue and Hillsdale Highway", "Southwest", "5AM-12AM", "Hillsdale Park", "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=799" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DogParks");
+        }
+    }
+}
--- DogParks/Models/DogParksContext.cs
+++ DogParks/Models/DogParksContext.cs
@@ -10,5 +10,47 @@ namespace DogParks.Models
         }
 
         public DbSet<DogPark> DogParks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<DogPark>().HasData(
+                new DogPark
+                {
+                    DogParkId = 1,
+                    Name = "Overlook Park",
+                    Area = "North",
+                    Address = "N Fremont Street and Interstate Avenue",
+                    Hours = "5AM-12AM",
+                    PhotoPath = "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=250"
+                },
+                new DogPark
+                {
+                    DogParkId = 2,
+                    Name = "Couch Park",
+                    Area = "Northwest",
+                    Address = "NW 19th Avenue and Glisan Street",
+                    Hours = "5AM-12AM",
+                    PhotoPath = "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=76"
+                },
+                new DogPark
+                {
+                    DogParkId = 3,
+                    Name = "Laurelhurst Park",
+                    Area = "Southeast",
+                    Address = "SE Cesar E Chavez Blvd and Stark Street",
+                    Hours = "5AM-10:30PM",
+                    PhotoPath = "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=115"
+                },
+                new DogPark
+                {
+                    DogParkId = 4,
+                    Name = "Hillsdale Park",
+                    Area = "Southwest",
+                    Address = "SW 27th Avenue and Hillsdale Highway",
+                    Hours = "5AM-12AM",
+                    PhotoPath = "https://www.portlandoregon.gov/parks/finder/index.cfm?action=ViewFile&PolPhotosID=799"
+                }
+            );
+        }
     }
 }
